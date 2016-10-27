8e7cbe1dd49919d3fc106cc29d4eb80b2a1456ea
András Kovács <alpintrekker@gmail.com>
2016-10-27 04:04:21 +0200

    basic function advances

--- AD5292.py
+++ AD5292.py
@@ -15,15 +15,13 @@ class AD5292:
 
     def _prepare_spi(self):
         self.curr_chip_settings = self.dev.chip_settings
-        for i in range(9):
-            if i == self.cs:
-                self.curr_chip_settings.pin_designations[i] = 1
-            else:
-                self.curr_chip_settings.pin_designations[i] = 0
+        self.curr_chip_settings.pin_designations[self.cs] = 1
         self.dev.chip_settings = self.curr_chip_settings
 
         self.curr_transfer_settings = self.dev.transfer_settings
         self.curr_transfer_settings.bit_rate = self.bit_rate
+        self.curr_transfer_settings.idle_cs = 1<<self.cs
+        self.curr_transfer_settings.active_cs = 0
         self.dev.transfer_settings = self.curr_transfer_settings
 
     def set_wiper(self, pos):
@@ -32,10 +30,15 @@ class AD5292:
         self.dev.transfer('\x18\x02')   # allow update of wiper position through digital Interface
         self.dev.transfer(
             struct.pack('>H',(1 << 10) | (pos & ((1<<10)-1))))   # convert to LE, aka > formatting character
-        self.dev.transfer('\x08\x00')   # prepare read back of RDAC
-        self.dev.transfer('\x00\x00')   # NoOp, last 10 bits of
-                                        # the returned 16 bit word is
-                                        # the actual wiper position
+
         # self.dev.transfer('')
         # raise NotImplementedError
 
+    def read_wiper(self):
+        self.dev.transfer('\x08\x00')   # prepare read back of RDAC
+        return struct.unpack('H', self.dev.transfer('\x00\x00'))
+        # NoOp, last 10 bits of the returned 16 bit word is the actual wiper position
+
+
+dgp = AD5292(dev, 5)
+dgp._prepare_spi()
--- accu.py
+++ accu.py
@@ -5,7 +5,102 @@ import time
 
 ser = serial.Serial('/dev/ttyUSB0', 38400)
 
-a = ser.read(ser.inWaiting())
+a = ''
+a += ser.read(ser.inWaiting())
+c = a.split('\xfe')
+c_ = []
+for i in range(len(c)):
+    c_.append(struct.unpack('B'*len(c[i]), c[i]))
+
+
+types = {
+    0b0001: {'text': 'V AC',
+             0b000: 3e-1,
+             0b001: 3,
+             0b010: 3e1,
+             0b011: 3e2,
+             0b100: 1e3},
+    0b0010: {'text': 'V AC+DC',
+             0b000: 3e-1,
+             0b001: 3,
+             0b010: 3e1,
+             0b011: 3e2,
+             0b100: 1e3},
+    0b0011: {'text': 'V DC',
+             0b000: 3e-1,
+             0b001: 3,
+             0b010: 3e1,
+             0b011: 3e2,
+             0b100: 1e3},
+    0b0100: {'text': 'Ohm'},
+    0b0101: {'text': 'Diode'},
+    0b0110: {'text': 'degC'},
+    0b0111: {'text': 'Farad'},
+    0b1000: {'text': 'mA DC',
+             0b000: 3e-4,
+             0b001: 3e-3,
+             0b010: 3e-2,
+             0b011: 3e-1
+             },
+    0b1001: {'text': 'A DC'},
+    0b1010: {'text': 'mA AC+DC'},
+    0b1011: {'text': 'A AC+DC'},
+    0b1100: {'text': 'Hz'},
+    0b1101: {'text': 'dB'},
+    0b1110: {'text': 'Events'},
+    0b1111: {'text': 'Uhr'},
+    0b0000: {'text': 'Leer'}
+}
+
+
+def decode_stream(list_of_packets_in_tuples_of_bytes):
+    measurement_type = None
+    measurement_range = None
+    sign = 1
+    for packet in list_of_packets_in_tuples_of_bytes:
+        packet_length = len(packet)
+
+        if packet_length > 0:
+            packet_type = (packet[-1] & 0b00110000) >> 4
+            if measurement_type:
+                if packet_length == 6:
+                    if packet_type == 2:  # measured data
+                        measured_data = assemble_decimal_data(packet)
+
+                        if ((packet[-1] & 0b00001000) >> 3) == 1:
+                            sign = -1
+                        else:
+                            sign = 1
+
+                        meassurement_range = (packet[-1] & 0b1111)
+
+                    if packet_type == 0:
+                        measured_data = assemble_decimal_data(packet)
+
+                    print 'Numbers:', measured_data * sign, 'Packet header:', bin(packet[-1])
+
+            if packet_length == 5:
+                if packet_type == 1:
+                    measurement_type = packet[-5] & 0b1111
+                    print types[measurement_type]['text'],
+
+                print '5 byte of something:',
+                for k in packet:
+                    print format(k, '#010b'), ' ',
+                print ''
+
+
+def assemble_decimal_data(data_packet):
+    measured_data = 0
+    for index in range(2, 7):
+        measured_data += (data_packet[-index] & 0b1111) * 10 ** (6 - index)
+    return measured_data
+
+
+
+
+
+
 b = struct.unpack('B'*len(a), a)
 b
 # for i in list(b):
