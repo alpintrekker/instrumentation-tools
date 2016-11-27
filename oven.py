9414042ec4dbbadf15aa6abde6349af23ec8e52d
András Kovács <alpintrekker@gmail.com>
2016-11-27 13:36:40 +0100

    some functions defined

--- oven.py
+++ oven.py
@@ -1,16 +1,20 @@
 import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.PWM as PWM
-
+import time
 psu_pin = "P9_15"
 fan_pin = "P9_14"
 H1_pin = "P8_13"
-H2_pin = "P8_19"
+H2_pin = "P9_42"
 
 def oven_init():
-    H1 = Heater(H1_pin, 1)
-    H2 = Heater(H2_pin, 1)
+    # H1 = Heater(H1_pin, 1)
+    # H2 = Heater(H2_pin, 1)
     fan_init()
     psu_on()
+
+def oven_cleanup():
+    PWM.cleanup()
+    GPIO.cleanup()
 
 def fan_init():
     PWM.start(fan_pin, 9, 100)
@@ -19,7 +23,16 @@ def fan_duty(duty):
     if 9.55 <= duty <= 18.0:
         PWM.set_duty_cycle(fan_pin, duty)
 
+def fan_min():
+    fan_duty(9.7)
+    time.sleep(0.5)
+    fan_duty(9.55)
+
+def fan_off():
+    PWM.set_duty_cycle(fan_pin, 9)
+
 def psu_on():
+    GPIO.setup(psu_pin, GPIO.OUT)
     GPIO.output(psu_pin, GPIO.HIGH)
 
 def psu_off():
